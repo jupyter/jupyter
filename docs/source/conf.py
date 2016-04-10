171dd4a735abfb4046dc4a5440cb345de9b72d68
Carol Willing <carolcode@willingconsulting.com>
2016-04-09 22:36:40 -0700

    Add support for jupyter sphinx theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,9 @@ import os
 import shlex
 import recommonmark.parser
 
+from jupyter_sphinx_theme import *
+init_theme()
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -304,12 +307,3 @@ intersphinx_mapping = {
     'jupytercore': ('https://jupyter-core.readthedocs.org/en/latest/', None),
 }
 
-# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-# otherwise, readthedocs.org uses their theme by default, so no need to specify it
