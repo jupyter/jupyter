79e3cebfb5046d2b309d37228c2a01c9376eeb3e
Carol Willing <carolcode@willingconsulting.com>
2016-05-01 14:09:02 -0700

    Tweak theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -93,6 +93,7 @@ todo_include_todos = False
 # a list of builtin themes.
 # html_theme = 'alabaster'
 
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
@@ -100,9 +101,7 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
-html_sidebars = {
-   '**': ['globaltoc.html', 'sourcelink.html', 'searchbox.html'],
-}
+
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
 html_additional_pages = {'index': 'index.html'}
