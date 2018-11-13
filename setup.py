a51860468d0d8757ea6c995eb4e79189a791f500
amisolution <siriusgem1@gmail.com>
2018-11-13 22:59:40 +0100

    Add Python v3.6

--- setup.py
+++ setup.py
@@ -44,7 +44,9 @@ setup_args = dict(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+        
     ],
 )
 
