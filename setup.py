bab2c62ca8cdb6980098dabdecba38520e9e0c64
amisolution <siriusgem1@gmail.com>
2018-11-13 23:29:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,6 @@ setup_args = dict(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
-        
     ],
 )
 
