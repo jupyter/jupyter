756b070bbde457d8e367ca7e7e13348ad2bb9870
Eric Muccino <33988506+emuccino@users.noreply.github.com>
2021-10-09 13:37:36 -0400

    Added programming language classifiers
    Added programming language classifiers to specify Python minor release compatibility for versions 3.6, 3.7, 3.8, and 3.9.

--- setup.py
+++ setup.py
@@ -44,7 +44,11 @@ setup_args = dict(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
     ],
 )
 
