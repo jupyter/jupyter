0ca85722ec16f03a420e732c7ebb183282c8051b
Jason Grout <jasongrout@users.noreply.github.com>
2025-11-07 14:22:02 -0800

    Remove JupyterCon banner announcement (#800)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -89,7 +89,6 @@ html_context = {
 }
 
 html_theme_options = {
-    "announcement": "🚀 Join us in San Diego · JupyterCon 2025 · Nov 4-5 · <a href=\"https://events.linuxfoundation.org/jupytercon/program/schedule/?ajs_aid=53afb00d-be65-4a99-9112-28cdaac99463\">SCHEDULE</a> · <a href=\"https://events.linuxfoundation.org/jupytercon/register/?ajs_aid=53afb00d-be65-4a99-9112-28cdaac99463\">REGISTER NOW</a>",
     "icon_links": [
         {
             "name": "GitHub",
